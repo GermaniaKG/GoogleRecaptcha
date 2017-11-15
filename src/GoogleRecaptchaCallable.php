e4dce9cd581a21fa6aacdd765b1ef29ea18a132f
Carsten Witt <carsten.witt@gmail.com>
2017-11-15 13:34:36 +0100

    First commit

---
+++ src/GoogleRecaptchaCallable.php
@@ -0,0 +1,60 @@
+<?php
+namespace Germania\GoogleRecaptcha;
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+use ReCaptcha\ReCaptcha;
+
+class GoogleRecaptchaCallable
+{
+    /**
+     * @var ReCaptcha
+     */
+    public $recaptcha;
+
+    /**
+     * @var LoggerInterface
+     */
+    public $logger;
+
+
+    /**
+     * @param ReCaptcha            $recaptcha
+     * @param LoggerInterface|null $logger
+     */
+    public function __construct( ReCaptcha $recaptcha, LoggerInterface $logger = null )
+    {
+        $this->recaptcha = $recaptcha;
+        $this->logger = $logger ?: new NullLogger;
+    }
+
+
+    /**
+     * @param  string $g_recaptcha_response
+     * @param  string $remote_ip
+     * @return bool
+     *
+     * See also: https://developers.google.com/recaptcha/docs/verify
+     */
+    public function __invoke( $g_recaptcha_response, $remote_ip)
+    {
+        $response = $this->recaptcha->verify($g_recaptcha_response, $remote_ip);
+
+
+        if ($response->isSuccess()) :
+            $this->logger->info( "Validation successful", [
+                'remote_ip' => $remote_ip
+            ]);
+            return true;
+        endif;
+
+
+        $errors = $response->getErrorCodes() ?: array();
+        $this->logger->notice( "Failed validation", [
+            'remote_ip' => $remote_ip,
+            'errors'    => join(",", $errors )
+        ]);
+
+        return false;
+    }
+}
